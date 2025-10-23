c6acc01ae50edb584bd8e7bc6fb6ac2107dd323f
xfy0708 <xiafeiyu666@qq.com>
2025-10-23 12:06:06 +0800

    1

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main()
+{
+	int a;
+	int b;
+	int sum;
+	printf("请输入整数a:");
+	scanf("%d",&a) ;
+	printf("请输入整数b:");
+	scanf("%d",&b) ;
+	sum=a+b;
+	printf("sum is:%d",sum);
+	return 0;
+}
